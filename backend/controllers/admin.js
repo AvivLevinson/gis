690d7252c541247b4ce26a7602afe667a3c0c70e
AvivL <avivlevinson1992@gmail.com>
2020-05-03 16:53:13 +0300

    test

--- backend/controllers/admin.js
+++ backend/controllers/admin.js
@@ -2,12 +2,8 @@ const routerData = require('../models/routerData');
 
 const utilsFunction = require('../utils/trace');
 
-exports.execTrace = async (req, res, next) => {
-  try {
-    utilsFunction.trace(req.body.url).then(console.log('execTrace'));
-  } catch (error) {
-    console.log('this is error from execTrace: ' + error);
-  }
+exports.execTrace = (req, res, next) => {
+  utilsFunction.trace(req.body.url);
   res.render('map', {
     routerData: routerData.router,
   });
